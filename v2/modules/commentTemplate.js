6e24a0d1091cbebedce2dc91b08af9463998e8e2
akashirohman <oktavianaziya@gmail.com>
2025-06-19 01:57:05 +0700

    Update commentTemplate.js

--- v2/modules/commentTemplate.js
+++ v2/modules/commentTemplate.js
@@ -1,12 +1,12 @@
+// modules/commentTemplate.js
+
 const fs = require('fs');
 const path = require('path');
-
-const templates = fs.readFileSync(path.join(__dirname, '../data/comment-templates.txt'), 'utf-8')
-  .split('\n').filter(Boolean);
+const templates = fs.readFileSync(path.join(__dirname, '../data/comment-templates.txt'), 'utf-8').split('\n').filter(Boolean);
 
 function getComment(targetUrl, anchorText) {
-  const random = templates[Math.floor(Math.random() * templates.length)];
-  return random.replace(/\{url\}/g, targetUrl).replace(/\{anchor\}/g, anchorText);
+  const t = templates[Math.floor(Math.random() * templates.length)];
+  return t.replace(/\{url\}/g, targetUrl).replace(/\{anchor\}/g, anchorText);
 }
 
 module.exports = { getComment };
